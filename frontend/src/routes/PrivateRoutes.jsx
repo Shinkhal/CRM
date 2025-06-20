e45f9bae95c5a26062ca575cd03b9d27e0fa5c98
Shinkhal <shinkhalsinha@gmail.com>
2025-06-21 01:42:05 +0530

    added validation of user for accessing some pages

--- frontend/src/routes/PrivateRoutes.jsx
+++ frontend/src/routes/PrivateRoutes.jsx
@@ -1,9 +1,12 @@
 import { Navigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const PrivateRoute = ({ children }) => {
-  const token = localStorage.getItem('token');
-  return token ? children : <Navigate to="/" />;
+  const { accessToken, loading } = useAuth();
+
+  if (loading) return <div className="p-6 text-center">Checking authentication...</div>;
+
+  return accessToken ? children : <Navigate to="/" />;
 };
 
 export default PrivateRoute;
-

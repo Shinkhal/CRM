a18e3616bb1b943aec48338ab6332ab55ee3afc0
Shinkhal <shinkhalsinha@gmail.com>
2025-06-21 01:48:16 +0530

    Callback for passport

--- frontend/src/pages/AuthSuccess.jsx
+++ frontend/src/pages/AuthSuccess.jsx
@@ -1,20 +1,23 @@
 import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useAuth } from '../context/AuthContext';
 
 const AuthSuccess = () => {
   const navigate = useNavigate();
+  const { setAccessToken } = useAuth(); // get from context
 
   useEffect(() => {
     const params = new URLSearchParams(window.location.search);
-    const token = params.get('token');
+    const accessToken = params.get('accessToken');
 
-    if (token) {
-      localStorage.setItem('token', token);
-      navigate('/customers');
-    } else {
-      navigate('/login');
+    if (accessToken) {
+  setAccessToken(accessToken);
+  setTimeout(() => navigate('/home'), 100);
+}
+ else {
+      navigate('/');
     }
-  }, []);
+  }, [setAccessToken, navigate]);
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-white text-gray-800 text-lg">

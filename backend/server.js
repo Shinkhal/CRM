e92e696f4f493c2c9e67fdb5dc801409a01fe621
Shinkhal Sinha <shinkhalsinha@gmail.com>
2025-08-16 16:44:09 +0530

    Added redis for caching and session

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import  connectDB  from './config/db.js';
+import connectDB from './config/db.js';
 import customerRoutes from './routes/customerRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
 import campaignRoutes from './routes/campaignRoutes.js';
@@ -13,36 +13,57 @@ import './config/passport.js';
 import authRoutes from './routes/authRoutes.js';
 import aiRoutes from './routes/aiRoutes.js';
 import dashboardRoutes from './routes/dashboardRoutes.js';
+import healthRoutes from './routes/health.js';
+
+import {RedisStore} from "connect-redis"
+import redisClient from './config/redis.js';
 
 dotenv.config();
 connectDB();
 
 const app = express();
 
+
+const redisStore = new RedisStore({
+  client: redisClient,
+  prefix: "CRM:",
+})
+
 app.use(cors({
   origin: 'https://crm-gamma-inky.vercel.app',
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
+app.use(cookieParser());
 
-app.get('/', (req, res) => {
-  res.send('API is running...');
-});
+app.use(
+  session({
+    store: redisStore,
+    secret: process.env.JWT_SECRET ,
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: process.env.NODE_ENV === 'production',
+      httpOnly: true,
+      maxAge: 1000 * 60 * 60 * 24, 
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use('/health', healthRoutes);
 app.use('/api/customers', customerRoutes);
 app.use('/api/orders', orderRoutes);
 app.use('/api/campaigns', campaignRoutes);
 app.use('/api/logs', logRoutes);
-app.use(cookieParser());
-app.use(session({ secret: process.env.JWT_SECRET, resave: false, saveUninitialized: true }));
-app.use(passport.initialize());
-app.use(passport.session());
 app.use('/api/ai', aiRoutes);
 app.use('/auth', authRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });

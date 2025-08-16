1f3a6fa560982ca0b26853a9295f47b25c97d5da
Shinkhal Sinha <shinkhalsinha@gmail.com>
2025-08-16 16:36:07 +0530

    Added redis for caching and scaling

---
+++ backend/config/redis.js
@@ -0,0 +1,22 @@
+// config/redis.js
+import { createClient } from "redis";
+import dotenv from 'dotenv';
+dotenv.config();
+
+const redisURL = process.env.REDIS_URL;
+
+const client = createClient({
+  url: redisURL,
+});
+
+client.on("connect", () => {
+  console.log("✅ Connected to Redis");
+});
+
+client.on("error", (err) => {
+  console.error("❌ Redis connection error:", err);
+});
+
+await client.connect();
+
+export default client;
